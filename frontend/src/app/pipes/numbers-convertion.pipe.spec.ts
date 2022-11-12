d714e813d63f5e5257d12a690873cb120e47c475
caiphus <caiphus@thedigitalacademy.co.za>
2022-11-12 08:21:38 +0200

    structure initiated

---
+++ frontend/src/app/pipes/numbers-convertion.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NumbersConvertionPipe } from './numbers-convertion.pipe';
+
+describe('NumbersConvertionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NumbersConvertionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
